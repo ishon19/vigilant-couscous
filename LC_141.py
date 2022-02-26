9723de7b959d7d45a350e8bebe7b416259365293
Shreyans Pathak <pathak.shreyans@gmail.com>
2022-02-26 00:18:12 -0500

    done: :tada: Accepted

---
+++ LC_141.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return False
+        fast, slow = head.next, head        
+        while slow!=fast:
+            if fast is None or fast.next is None:
+                return False
+            fast = fast.next.next
+            slow = slow.next
+        return True
