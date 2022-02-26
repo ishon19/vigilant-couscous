bacb521a86befcf51caf63f8809735564828923b
Shreyans Pathak <pathak.shreyans@gmail.com>
2022-02-26 00:19:37 -0500

    done: :tada: Accepted

---
+++ LC_206.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+            
