270f153b0d760968f3d30519365bb621408a748e
Shreyans Pathak <ishon19@users.noreply.github.com>
2022-06-19 20:16:10 -0400

    Time: 147 ms (69.25%), Space: 17.1 MB (84.12%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,23 +1,32 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+#         products.sort()
+#         # print(products)
+#         ans = []
+#         MAX_RES = 3
+        
+#         # create the typed words
+#         typedWords = ''
+#         typedWordList = []
+#         for word in searchWord:
+#             typedWords += word
+#             typedWordList.append(typedWords)
+        
+#         for word in typedWordList:
+#             i = MAX_RES
+#             res = []
+#             for product in products:            
+#                 if word in product and product.startswith(word) and i>0:    
+#                     res.append(product)
+#                     i -= 1            
+#             ans.append(res)           
+#         return ans
+        
+        # alternate solution 60 ms
         products.sort()
-        # print(products)
-        ans = []
-        MAX_RES = 3
-        
-        # create the typed words
-        typedWords = ''
-        typedWordList = []
-        for word in searchWord:
-            typedWords += word
-            typedWordList.append(typedWords)
-        
-        for word in typedWordList:
-            i = MAX_RES
-            res = []
-            for product in products:            
-                if word in product and product.startswith(word) and i>0:    
-                    res.append(product)
-                    i -= 1            
-            ans.append(res)           
-        return ans
+        res, cur = [], ''
+        for c in searchWord:
+            cur += c
+            i = bisect.bisect_left(products, cur)
+            res.append([w for w in products[i:i+3] if w.startswith(cur)])
+        return res
