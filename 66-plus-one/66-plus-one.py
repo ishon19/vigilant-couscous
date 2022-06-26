3a0cd54657b912aaafb1738f2627b57279d89de4
Shreyans Pathak <ishon19@users.noreply.github.com>
2022-06-26 11:13:58 -0400

    Time: 58 ms (25.87%), Space: 14 MB (11.07%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,7 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        nums = [str(i) for i in digits]
-        numstr = "".join(nums)
-        ans = (int(numstr) + 1)
-        return [int(x) for x in str(ans)]
+        digits[-1] += 1 
+        
+        for i in reversed(range(1, len(digits))):
+            if digits[i] != 10:
+                break
+            digits[i] = 0
+            digits[i-1] += 1
+        
+        if digits[0] == 10:
+            digits[0] = 1
+            digits.append(0)
+        
+        return digits
         
