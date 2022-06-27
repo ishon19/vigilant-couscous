8ca15aae5943059fe31bed8c935f18944bd62bed
Shreyans Pathak <ishon19@users.noreply.github.com>
2022-06-27 00:30:09 -0400

    Time: 83 ms (5.17%), Space: 14 MB (20.92%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashtable = {key:s.count(key) for key in s}
+        if len(hashtable) == 1:
+            return len(s)
+        print(hashtable)
+        max_len = 0        
+        seen = False
+        for key in hashtable:
+            max_len += hashtable[key]//2 * 2
+            if max_len%2==0 and hashtable[key]%2==1:
+                max_len += 1        
+        return max_len
