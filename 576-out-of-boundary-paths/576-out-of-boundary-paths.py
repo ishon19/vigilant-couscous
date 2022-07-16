e9221a3a1424bc6eb92b3a6e3fb052613a5dae45
Shreyans Pathak <ishon19@users.noreply.github.com>
2022-07-16 19:12:57 -0400

    Time: 106 ms (94.12%), Space: 20.3 MB (17.39%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -3,15 +3,18 @@ class Solution:
         
         @lru_cache(None)
         def dfs(moves,i,j):
+            # bounds check
             if (i==m or j==n or i<0 or j<0):
                 return 1
+            # max moves check
             elif moves==0:
                 return 0
+            # perform dfs in all the directions
             ans = 0
             ans +=  (dfs(moves-1,i - 1, j) + dfs(moves-1,i + 1, j) + dfs(moves-1,i, j - 1) + dfs(moves-1,i, j + 1))%(1000000007)
             return ans
         
-        return dfs(maxMove, startRow, startColumn)        
+        return dfs(maxMove, startRow, startColumn)%(1000000007)
         
         
         
