ed09c617b13de0ae5451d0df96454643432f7bc9
Shreyans Pathak <ishon19@users.noreply.github.com>
2022-08-06 16:23:34 -0400

    Time: 41 ms (67.15%), Space: 14 MB (20.44%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        return math.ceil(math.log(buckets)/math.log(minutesToTest//minutesToDie+1))
+    
