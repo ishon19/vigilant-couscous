8e94759779871d68fa990dda5b3a343c45e83927
Shreyans Pathak <ishon19@users.noreply.github.com>
2023-01-21 20:10:57 +0530

    Time: 84 ms (13.17%), Space: 13.9 MB (31.50%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        def dfs(s, idx, path, res):
+            if idx > 4:
+                return 
+            if idx == 4 and not s:
+                res.append(path[:-1])
+                return 
+            for i in range(1, len(s)+1):
+                if s[:i]=='0' or (s[0]!='0' and 0 < int(s[:i]) < 256):
+                    dfs(s[i:], idx+1, path+s[:i]+".", res)
+        res = []
+        dfs(s, 0, "", res)
+        return res
