1de08d603414de9b9cbbbab3616f134a79ba28e9
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-01-08 12:21:23 -0500

    Time: 1523 ms (5.01%) | Memory: 19.1 MB (69.41%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -1,8 +1,8 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         res = 0
+        s.sort()
         for greed in g:
-            s.sort()
             for i in range(len(s)):
                 if s[i] >= greed:
                     del s[i]
