c33aaab396c0f423a66635c0c61c60ff9e436b31
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-02-14 09:32:20 +0530

    Time: 1288 ms (76.35%) | Memory: 20.6 MB (74.62%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -10,8 +10,9 @@ class SparseVector:
         if not v1 and not v2: return 0
         if v1 and not v2: return v1
         if v2 and not v1: return v2
+        pairs = [v1[i] * v2[i] for i in range(len(v1)) if v1 and v2]
         
-        return sum(i * j for i, j in zip(v1, v2))
+        return sum(pairs)
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
