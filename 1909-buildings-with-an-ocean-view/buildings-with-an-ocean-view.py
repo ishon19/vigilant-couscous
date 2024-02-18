73d0f0faddedfc05ea8b162709513f61bf7222f2
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-02-18 14:22:17 +0530

    Time: 526 ms (40.69%) | Memory: 34.2 MB (45.82%) - LeetSync

---
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        res = []
+        stack = []
+
+        for idx, height in reversed(list(enumerate(heights))):
+            if not stack:
+                stack.append(height)
+                res.append(idx)
+            else:
+                if height > stack[-1]:
+                    stack.pop()
+                    stack.append(height)
+                    res.append(idx)
+        
+        return sorted(res)
