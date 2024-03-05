aec44e57c86cd8f58337c76e626423e59740fdba
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-03-05 09:20:09 +0530

    Time: 70 ms (57.35%) | Memory: 17.4 MB (75.83%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -5,17 +5,14 @@ class Solution:
 
         l, r = 0, len(s) - 1
 
-        while l < r:
-            if s[l] != s[r]:
-                return r - l + 1
-            else:
-                look = s[l]
+        while l < r and s[l] == s[r]:
+            look = s[l]
 
-                while l < len(s) and s[l] == look:
-                    l += 1
+            while l < len(s) and s[l] == look:
+                l += 1
 
-                while r >= 0 and s[r] == look:
-                    r -= 1
+            while r >= 0 and s[r] == look:
+                r -= 1
         
         return max(0, r - l + 1)
                 
