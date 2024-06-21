d778a726964312294c016fbc2b592ad15ae2a644
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-06-21 10:08:22 -0400

    Time: 1640 ms (5.01%) | Memory: 16.5 MB (27.80%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,9 +1,20 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
+        def search(num):
+            l, r = 0, num
+            while l <= r:
+                mid = (l + r) // 2
+                if mid * mid == num:
+                    return True
+                elif mid * mid > num:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            
         a = 0
         while a * a <= c:
-            b = math.sqrt(c - a * a)
-            if b == int(b):
+            b = c - a * a # check if c - a^2 is perfect square
+            if search(b):
                 return True
             a += 1
         return False
