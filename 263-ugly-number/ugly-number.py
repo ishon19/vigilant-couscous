b82cbf9f80c103769dc010d1bd033dfb7e2db6e6
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-08-19 07:45:53 -0400

    Time: 35 ms (65.66%) | Memory: 16.5 MB (46.36%) - LeetSync

---
+++ 263-ugly-number/ugly-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n<=0: return False
+
+        for factor in [2,3,5]:
+            while n % factor == 0:
+                # print(factor, n)
+                n //= factor
+        return n == 1
