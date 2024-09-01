0f5a2422de18c7b4351e3c90a63d5682798f6e41
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-09-01 18:40:17 -0400

    Time: 699 ms (23.78%) | Memory: 24.5 MB (19.35%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,10 +1,26 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        res = []
-        if m * n == len(original):
-            i, j = 0, n-1
-            while len(res) != m:
-                res.append(original[i:j+1])
-                i += n
-                j += n
-        return res
+        # res = []
+        # if m * n == len(original):
+        #     i, j = 0, n-1
+        #     while len(res) != m:
+        #         res.append(original[i:j+1])
+        #         i += n
+        #         j += n
+        # return res
+
+        rows, cols = m, n
+        if m * n != len(original):
+            return []
+
+        grid = []
+        idx = 0
+        for i in range(rows):
+            row = []
+            if idx >= len(original): break
+            for j in range(cols):
+                if idx + j < len(original): row.append(original[idx + j])
+            idx += cols
+            grid.append(row)
+        
+        return grid
