3d153f1650ef30c157e9931cf35020078d000bd2
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-09-02 21:32:58 -0400

    Time: 30 ms (94.26%) | Memory: 16.5 MB (37.00%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,7 +1,6 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        num_str = ''.join(str(ord(c)-96) for c in s)
-        print(num_str)
+        num_str = ''.join(str(ord(c)-96) for c in s)    
         res = 0
         while k:
             localSum = sum(int(c) for c in num_str)
