f6283bd1ac0fd11d085294d749d24f5f7366b7be
Shreyans Pathak <ishon19@users.noreply.github.com>
2024-09-09 21:03:54 -0400

    Time: 63 ms (85.82%) | Memory: 19.6 MB (79.95%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
@@ -6,11 +6,8 @@
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         temp = ListNode(-1, head)
-
         ptr = head
         while ptr.next:
-            new_node = ListNode(math.gcd(ptr.val,  ptr.next.val), ptr.next)
-            ptr.next = new_node
+            ptr.next = ListNode(math.gcd(ptr.val,  ptr.next.val), ptr.next)
             ptr = ptr.next.next
-        
         return temp.next
