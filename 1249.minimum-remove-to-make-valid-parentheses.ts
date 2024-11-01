69b9a419afa5ac23464984c82a8e3e406b8ad43b
Shreyans Pathak <pathak.shreyans@gmail.com>
2024-11-01 12:58:32 -0400

    local changes

---
+++ 1249.minimum-remove-to-make-valid-parentheses.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1249 lang=typescript
+ *
+ * [1249] Minimum Remove to Make Valid Parentheses
+ */
+
+// @lc code=start
+function minRemoveToMakeValid(s: string): string {
+    const res = "";
+    const stack = [];
+    for (const c of s) {
+      if(c == '(')
+  }
+  return res;
+}
+// @lc code=end
---
+++ 3048.earliest-second-to-mark-indices-i.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=3048 lang=typescript
+ *
+ * [3048] Earliest Second to Mark Indices I
+ */
+
+// @lc code=start
+function earliestSecondToMarkIndices(nums: number[], changeIndices: number[]): number {
+    
+};
+// @lc code=end
+
