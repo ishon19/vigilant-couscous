7c28650fd463f0a431c56b968d43839268d5d49d
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-01-12 03:08:46 -0500

    Time: 18 ms (10.13%) | Memory: 18.2 MB (34.78%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,22 @@
 class Solution:
+    def isValid(self, letter):
+        return ord('a')<=ord(letter)<=ord('z') or ord('0')<=ord(letter)<=ord('9')
+
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join(c.lower() if c.isalpha() or c.isnumeric() else '' for c in s)
-        return s == s[::-1]
+        l, r = 0, len(s) - 1
+        s = s.lower()
+
+        while l <= r:
+            if self.isValid(s[l]) and self.isValid(s[r]):
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            elif self.isValid(s[l]):
+                r -= 1
+            else:
+                l += 1
+        
+        return True
+            
