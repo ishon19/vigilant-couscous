1562e8f9f12ccad65152e52d2f5f38aaf946368e
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-01-18 08:40:53 -0500

    Time: 3 ms (35.42%) | Memory: 19.8 MB (37.17%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,15 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # 29 Nov 2024, 8.16 PM 
-        def dfs(node, min_val, max_val):
+    def isValidBST(self, root: Optional[TreeNode], low=inf, high=-inf) -> bool:
+        # 9th Dec 2024, 5.44 PM
+        if not root:
+            return True
+        
+        def dfs(node, left, right):
             if not node:
-                return True            
-            if not min_val < node.val < max_val:
-                return False            
-            return dfs(node.left, min_val, node.val) and dfs(node.right, node.val, max_val)        
-        return dfs(root, -inf, inf)        
+                return True
+            
+            if node.val <= left or node.val >= right:
+                return False
+            
+            return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
+        
+        return dfs(root, -inf, inf)
+            
+        # 29 Nov 2024, 8.16 PM 
+        # def dfs(node, min_val, max_val):
+        #     if not node:
+        #         return True            
+        #     if not min_val < node.val < max_val:
+        #         return False            
+        #     return dfs(node.left, min_val, node.val) and dfs(node.right, node.val, max_val)        
+        # return dfs(root, -inf, inf)        
         # res = []
         # def inorder(node):
         #     nonlocal res
