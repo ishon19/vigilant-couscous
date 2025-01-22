bce8df4ac27a59837cb27a1c5328b7af347abb3f
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 18:37:44 -0500

    feat: add solution for "Compare Version Numbers" problem by implementing version comparison logic to handle different version formats

---
+++ 165.compare-version-numbers.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=165 lang=python3
+#
+# [165] Compare Version Numbers
+#
+
+# @lc code=start
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1_list = [int(revision) for revision in version1.split(".")]
+        v2_list = [int(revision) for revision in version2.split(".")]
+
+        if len(v1_list) < len(v2_list):
+            for i in range(len(v2_list) - len(v1_list)):
+                v1_list.append(0)
+        else:
+            for i in range(len(v1_list) - len(v2_list)):
+                v2_list.append(0)
+        
+        i = 0
+        while i < len(v1_list):
+            if v1_list[i] < v2_list[i]:
+                return -1
+            elif v1_list[i] > v2_list[i]:
+                return 1            
+            i += 1
+        
+        return 0
+        
+# @lc code=end
+
