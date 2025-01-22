9b07bed8d80051f14cfe9e674498525f0f98cb68
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 18:37:58 -0500

    feat: implement Trie (Prefix Tree) data structure with insert, search, and startsWith methods for efficient word storage and retrieval

---
+++ 208.implement-trie-prefix-tree.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=208 lang=python3
+#
+# [208] Implement Trie (Prefix Tree)
+#
+
+# @lc code=start
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
+
