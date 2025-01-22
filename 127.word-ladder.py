a286d8e1be8d32e5f3f9fb55637a169bb3acb55d
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 18:37:07 -0500

    feat: add solution for "Word Ladder" problem using breadth-first search

---
+++ 127.word-ladder.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=127 lang=python3
+#
+# [127] Word Ladder
+#
+
+# @lc code=start
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        res = 0
+        wordSet = set(wordList)
+
+        if endWord not in wordSet:
+            return 0
+        
+        q = deque([(beginWord, 1)])
+        while q:
+            word, length = q.popleft()
+
+            if word == endWord:
+                return length
+            
+            for i in range(len(word)):
+                for char in 'abcdefghijklmnopqrstuvwxyz':
+                    newWord = word[:i] + char + word[i+1:]
+                    if newWord in wordSet:
+                        wordSet.remove(newWord)
+                        q.append((newWord, length + 1))
+        
+        return 0
+
+# @lc code=end
+
