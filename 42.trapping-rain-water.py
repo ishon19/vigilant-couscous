9b9defe07f3efb748a8789c412b44ea6ec602c30
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 18:36:07 -0500

    feat: add solution for "Trapping Rain Water" problem

---
+++ 42.trapping-rain-water.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=42 lang=python3
+#
+# [42] Trapping Rain Water
+#
+
+# @lc code=start
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        maxl, maxr = height[l], height[r]
+        res = 0
+
+        while l <= r:
+            if height[l] < height[r]:
+                maxl = max(maxl, height[l])
+                res += maxl - height[l]
+                l += 1
+            else:
+                maxr = max(maxr, height[r])
+                res += maxr - height[r]
+                r -= 1
+        
+        return res
+        
+# @lc code=end
+
