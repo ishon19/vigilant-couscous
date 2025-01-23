d37156acb8c4121dacd148ac0dcbdb603b73548b
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 20:17:21 -0500

    feat: add initial implementation of "Check if Point Is Reachable" problem with class structure and method definition for determining if a target point is reachable

---
+++ 2543.check-if-point-is-reachable.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=2543 lang=python3
+#
+# [2543] Check if Point Is Reachable
+#
+
+# @lc code=start
+class Solution:
+    def isReachable(self, targetX: int, targetY: int) -> bool:
+        dirs = [(0, )]
+# @lc code=end
