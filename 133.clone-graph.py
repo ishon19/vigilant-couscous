882c958f49f513c1771aa6117a55629367902e78
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 21:31:19 -0500

    feat: add initial implementation of "Maximum Product Subarray" problem with class structure and method definition for calculating the maximum product of a contiguous subarray

---
+++ 133.clone-graph.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        
+        clones = {}
+
+        def dfs(node):
+            if node in clones:
+                return clones[node]
+            
+            clonedNode = Node(node.val)
+            clones[node] = clonedNode
+
+            for neighbor in node.neighbors:
+                clonedNode.neighbors.append(dfs(neighbor))
+            
+            return clonedNode
+        
+        return dfs(node)
+# @lc code=end
