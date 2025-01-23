ff7054d643be08f35572b5c6673c772de2155ef3
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 21:53:28 -0500

    feat: add initial implementation of "Walls and Gates" problem with class structure and method definition for modifying rooms in-place

---
+++ 286.walls-and-gates.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=286 lang=python3
+#
+# [286] Walls and Gates
+#
+
+# @lc code=start
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        
+# @lc code=end
+
