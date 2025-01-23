e11ed1236208c2a3866b2b5f63e75bbfafe4b869
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 20:14:50 -0500

    feat: add initial implementation for "Number of Islands II" problem with class structure and method definition

---
+++ 305.number-of-islands-ii.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=305 lang=python3
+#
+# [305] Number of Islands II
+#
+
+# @lc code=start
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        
+# @lc code=end
+
