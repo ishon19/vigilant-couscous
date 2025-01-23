0d8bdf51f733f261c4a559795c052e7b1da09628
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 20:16:37 -0500

    feat: add initial implementation for "Maximum Number of Events That Can Be Attended" problem with class structure and method definition for calculating the maximum number of events that can be attended

---
+++ 1353.maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1353 lang=python3
+#
+# [1353] Maximum Number of Events That Can Be Attended
+#
+
+# @lc code=start
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        
+# @lc code=end
+
