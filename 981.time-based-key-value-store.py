7ac7d238e8a3731a5a8988ce5dd8317c94f009e7
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 20:16:22 -0500

    feat: add initial implementation of "Time Based Key-Value Store" problem with class structure and method definitions for setting and getting values based on timestamps

---
+++ 981.time-based-key-value-store.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=981 lang=python3
+#
+# [981] Time Based Key-Value Store
+#
+
+# @lc code=start
+class TimeMap:
+
+    def __init__(self):
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+# @lc code=end
+
