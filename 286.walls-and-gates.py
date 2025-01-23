f682c7b0798d8da7050893d3e1ebe0f340fd53fb
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-23 05:54:34 -0500

    feat: implement BFS approach for "Walls and Gates" problem to modify grid in-place, calculating distances from gates

--- 286.walls-and-gates.py
+++ 286.walls-and-gates.py
@@ -6,10 +6,28 @@
 
 # @lc code=start
 class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+    def wallsAndGates(self, grid: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
+        # since this is a closest distance problem, we would do a 
+        # bfs based traversal.
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        seen = set()
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 0:
+                    q.append((row, col))
         
+        while q:
+            row, col = q.popleft()
+            seen.add((row, col))
+            for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr, nc = row + dr, col + dc
+                if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 2**31 - 1 and (nr, nc) not in seen:
+                    grid[nr][nc] = 1 + grid[row][col] 
+                    q.append((nr, nc))
 # @lc code=end
 
