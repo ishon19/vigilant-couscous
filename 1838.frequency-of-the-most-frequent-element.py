8da503359550290ccaa6f54c07af47e45a756f1b
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-01-22 20:16:57 -0500

    feat: add initial implementation of "Frequency of the Most Frequent Element" problem with class structure and method definition for calculating the maximum frequency of an element in an array

---
+++ 1838.frequency-of-the-most-frequent-element.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1838 lang=python3
+#
+# [1838] Frequency of the Most Frequent Element
+#
+
+# @lc code=start
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
