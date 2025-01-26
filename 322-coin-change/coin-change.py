892393fe01589b7724714041b936c2b95cfb0eb9
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-01-26 05:47:53 -0500

    Time: 707 ms (80.68%) | Memory: 18.1 MB (71.15%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -3,19 +3,28 @@ class Solution:
         self.memo = {}
     
     def helper(self, amount):
-        if amount == 0:
-            return 0
-        if amount < 0:
-            return inf
-        if amount in self.memo:
-            return self.memo[amount]
+        # if amount == 0:
+        #     return 0
+        # if amount < 0:
+        #     return inf
+        # if amount in self.memo:
+        #     return self.memo[amount]
         
-        result = inf
-        for coin in self.coins:
-            result = min(result, 1 + self.helper(amount - coin))
+        # result = inf
+        # for coin in self.coins:
+        #     result = min(result, 1 + self.helper(amount - coin))
         
-        self.memo[amount] = result
-        return result
+        # self.memo[amount] = result
+        # return result
+        dp = [inf] * (amount+1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in self.coins:
+                if i >= coin:
+                    dp[i] = min(dp[i], 1 + dp[i-coin])
+        
+        return dp[amount] if dp[amount] != inf else -1
 
 
     def coinChange(self, coins: List[int], amount: int) -> int:
