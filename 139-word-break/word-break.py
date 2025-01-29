b0d5ce4e8630addfde0f36ac67109eb31cdac92c
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-01-29 01:49:51 -0500

    Time: 4 ms (41.34%) | Memory: 18.6 MB (6.24%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,13 +1,52 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isWord = True
+    
+    def searchPrefix(self, s, start):
+        result = []
+        node = self.root
+        for i in range(start, len(s)):
+            char = s[i]
+            if char not in node.children:
+                break
+            node = node.children[char]
+            if node.isWord:
+                result.append(i+1)
+        return result
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def dfs(start_index):
-            if start_index == len(s):
-                return True
+        trie = Trie()
+
+        for word in wordDict:
+            trie.insert(word)
+        
+        n = len(s)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(n+1):
+            if not dp[i]:
+                continue
             
-            match = False
-            for word in wordDict:
-                if s[start_index:].startswith(word):
-                    match = match or dfs(start_index + len(word))
-            return match
-        return dfs(0)
+            for end in trie.searchPrefix(s, i):
+                dp[end] = True
+        
+        return dp[n]
+
+        t = 0
+
+        
