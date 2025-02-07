682397303e17b8d1b05f8172dc910ccf759c0c34
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-07 05:05:12 -0500

    feat: add initial implementation of "Jump Game II" problem with greedy approach for minimum jumps

---
+++ 45.jump-game-ii.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=45 lang=python3
+#
+# [45] Jump Game II
+#
+
+# @lc code=start
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+        
+        return jumps
+
+# @lc code=end
+
