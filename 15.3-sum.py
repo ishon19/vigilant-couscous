d4cb1a4dc7264af11341fee792a3efb0e86b40d1
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-08 07:53:33 -0500

    feat: add initial implementation of "3Sum" problem to find all unique triplets that sum to zero

---
+++ 15.3-sum.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    r -= 1
+        
+        return res
+
+# @lc code=end
+
