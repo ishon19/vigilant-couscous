5e9bd6fc7bdd2cb3117c3067bc821335499f601c
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-08 07:57:30 -0500

    feat: add initial implementation of "Check if There is a Valid Path in a Grid" problem

---
+++ 1391.check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1391 lang=python3
+#
+# [1391] Check if There is a Valid Path in a Grid
+#
+
+# @lc code=start
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        
+# @lc code=end
+
