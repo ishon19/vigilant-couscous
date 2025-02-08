50300fe079c514449280bd63c16acda2652d8c97
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-08 07:55:05 -0500

    feat: add initial implementation of "Non-overlapping Intervals" problem

---
+++ 435.non-overlapping-intervals.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=435 lang=python3
+#
+# [435] Non-overlapping Intervals
+#
+
+# @lc code=start
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+# @lc code=end
+
