fa31e8f0d9b394b505455fc34cbf0805620bb8a4
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-08 07:58:03 -0500

    feat: add initial implementation of "Count Pairs Of Similar Strings" problem

---
+++ 2506.count-pairs-of-similar-strings.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2506 lang=python3
+#
+# [2506] Count Pairs Of Similar Strings
+#
+
+# @lc code=start
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        
+# @lc code=end
+
