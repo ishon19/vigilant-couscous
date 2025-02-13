de12e8d5a5f22d8c869235177ad237e7f2407038
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-13 03:06:33 -0500

    feat: optimize addTwoNumbers function for clarity and performance

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -12,25 +12,24 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        p1, p2 = l1, l2
-
-        carry = 0
         dummy = ListNode(0)
-        res = dummy
+        ptr = dummy
+        carry = 0
 
-        while p1 or p2 or carry:
-            val1 = p1.val if p1 else 0
-            val2 = p2.val if p2 else 0
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
-            total = val1 + val2 + carry
+            total = x + y + carry
             carry = total // 10
-            val = total % 10
-            res.next = ListNode(val)
 
-            res = res.next
-            p1 = p1.next if p1 else None
-            p2 = p2.next if p2 else None
+            ptr.next = ListNode(total % 10)
+            ptr = ptr.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         
         return dummy.next
+
 # @lc code=end
 
