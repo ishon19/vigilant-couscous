8778c5f806b4acf7fe89dddafebbc9db72fc7cb1
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-15 19:53:07 -0500

    feat: add initial implementation of "Majority Element" problem

---
+++ 169.majority-element.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate
+
+# @lc code=end
+
