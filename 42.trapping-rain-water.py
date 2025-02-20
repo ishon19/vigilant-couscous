b03a43593329a7527dadd1edf0c2c16906c522ec
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-20 10:08:42 -0500

    feat: optimize trapping rain water solution by renaming variables and improving readability

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -7,21 +7,21 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        maxl, maxr = height[l], height[r]
-        res = 0
+       l, r = 0, len(height)-1
+       trapped = 0
+       maxl, maxr = height[l], height[r]
 
-        while l <= r:
-            if height[l] < height[r]:
-                maxl = max(maxl, height[l])
-                res += maxl - height[l]
-                l += 1
-            else:
-                maxr = max(maxr, height[r])
-                res += maxr - height[r]
-                r -= 1
-        
-        return res
+       while l < r:
+           if height[l] < height[r]:
+               maxl = max(maxl, height[l])
+               trapped += maxl - height[l]
+               l += 1
+           else:
+               maxr = max(maxr, height[r])
+               trapped += maxr - height[r] 
+               r -= 1
+       
+       return trapped
         
 # @lc code=end
 
