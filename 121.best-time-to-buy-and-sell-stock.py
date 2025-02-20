8bfcd358f0fe4108af50e93e0d8d7af71d7721f9
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-20 10:09:29 -0500

    feat: refactor maxProfit method to improve variable naming and clarity

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -5,15 +5,21 @@
 #
 
 # @lc code=start
+
+max_profit = 0
+min_so_far = 1
+price = 5
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        max_profit = 0
         min_so_far = float("inf")
 
         for price in prices:
             min_so_far = min(min_so_far, price)
-            res = max(res, price - min_so_far)
+            max_profit = max(max_profit, price - min_so_far)
         
-        return res
+        return max_profit
+
 # @lc code=end
 
