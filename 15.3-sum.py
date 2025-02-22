b876b2c44ed6baaddbff122a01d5a7290279db7b
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-21 19:58:42 -0500

    fix: optimize variable usage and formatting in threeSum implementation

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -9,27 +9,28 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        
-        for i in range(len(nums) - 2):
+
+        for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-                
-            l, r = i + 1, len(nums) - 1
+
+            l, r = i + 1, len(nums)-1
             while l < r:
-                total = nums[i] + nums[l] + nums[r]
+                total = nums[l] + nums[i] + nums[r]
                 if total == 0:
                     res.append([nums[i], nums[l], nums[r]])
-                    while l < r and nums[l] == nums[l + 1]:
+
+                    while l < r and nums[l] == nums[l+1]:
                         l += 1
-                    while l < r and nums[r] == nums[r - 1]:
+                    while l < r and nums[r] == nums[r-1]:
                         r -= 1
+
                     l += 1
                     r -= 1
                 elif total < 0:
                     l += 1
                 else:
                     r -= 1
-        
         return res
 
 # @lc code=end
