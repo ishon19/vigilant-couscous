b88727c27360419b624c8402c0bf1551dffcb022
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-02-21 19:58:30 -0500

    feat: add initial implementation of "Container With Most Water" problem

---
+++ 11.container-with-most-water.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=11 lang=python3
+#
+# [11] Container With Most Water
+#
+
+# @lc code=start
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+# @lc code=end
+
