8c07de0fe25f2a8d4589501f053a9a52027a881b
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:15:46 -0400

    refactor: improve removeNthFromEnd function with edge case handling

--- 19.remove-nth-node-from-end-of-list.py
+++ 19.remove-nth-node-from-end-of-list.py
@@ -12,20 +12,24 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(-1, head)
-        slow, fast = dummy, head
+        if not head:
+            return None
+        
+        slow = fast = head
 
         while n:
             fast = fast.next
             n -= 1
         
-        while fast:
+        if not fast:
+            return head.next
+        
+        while fast.next:
             slow = slow.next
             fast = fast.next
         
         slow.next = slow.next.next
-
-        return dummy.next
-        
+        return head
+    
 # @lc code=end
 
