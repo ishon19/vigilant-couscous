6fbbc7860c2d37381eabb743a60e6d355b442259
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:16:08 -0400

    feat: simplify insert intervals logic with improved readability and efficiency

--- 57.insert-interval.py
+++ 57.insert-interval.py
@@ -8,24 +8,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        i = 0
-        n = len(intervals)
+        
+        for start, end in intervals:
+            new_start, new_end = newInterval
 
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
-        
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        
+            if end < new_start:
+                res.append([start, end])
+            elif new_end < start:
+                res.append([new_start, new_end])
+                newInterval = [start, end]
+            else:
+                newInterval = [
+                    min(start, new_start), max(end, new_end)
+                ]
+                
         res.append(newInterval)
-
-        while i < n:
-            res.append(intervals[i])
-            i += 1
-        
         return res
 # @lc code=end
 
