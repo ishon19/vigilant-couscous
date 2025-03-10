9b39ac60819b32866e207211e1236a4ef105fd61
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:15:20 -0400

    refactor: inline dfs function for diameter calculation in binary tree

--- 543.diameter-of-binary-tree.py
+++ 543.diameter-of-binary-tree.py
@@ -12,24 +12,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self) -> None:
-        self.diameter = 0
-    
-    def dfs(self, node):
-        if not node:
-            return 0
-        
-        ldepth = self.dfs(node.left)
-        rdepth = self.dfs(node.right)
-        self.diameter = max(self.diameter, ldepth + rdepth)
-        
-        return 1 + max(ldepth, rdepth)
-
     def diameterOfBinaryTree(self, root: Optional[TreeNode], total=0) -> int:
         # the diameter of a binary tree is the longest path from one node 
         # to the other and so from one node, we look to the left and look to the right
         # the largest sum of the left and right depths are the answer
-        self.dfs(root)
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.diameter = max(self.diameter, left + right)
+
+            return 1 + max(left, right)
+        
+        self.diameter = 0
+        dfs(root)
         return self.diameter
 
 # @lc code=end
