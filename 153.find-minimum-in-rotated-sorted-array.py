dfc5bd105894b9489e07b3ea969989f5e8da5e95
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:19:16 -0400

    refactor: remove unused code from findMin method in rotated sorted array solution

--- 153.find-minimum-in-rotated-sorted-array.py
+++ 153.find-minimum-in-rotated-sorted-array.py
@@ -7,17 +7,6 @@
 # @lc code=start
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        res = nums[0]
-
-        while l < r:
-            mid = (l + r) // 2
-
-            if nums[mid] > nums[r]:
-                l = mid + 1
-            else:
-                r = mid
-
-        return nums[l]
+        
 # @lc code=end
 
