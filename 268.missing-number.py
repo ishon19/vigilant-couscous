4d2c55bf5165ea5daffec2ed9dd6396ebaba5c6f
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:19:43 -0400

    feat: implement optimized solution for finding the missing number

--- 268.missing-number.py
+++ 268.missing-number.py
@@ -7,9 +7,15 @@
 # @lc code=start
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        actual_total = sum(nums)
-        expected_total = (n * (n + 1)) // 2
-        return expected_total - actual_total        
+        # n = len(nums)
+        # actual_total = sum(nums)
+        # expected_total = (n * (n + 1)) // 2
+        # return expected_total - actual_total    
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res ^= nums[i] ^ i
+        
+        return res
 # @lc code=end
 
