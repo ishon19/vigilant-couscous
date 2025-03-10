ce19640d7c27e4e1bcdca3c0eb3aba63db499712
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-10 19:15:26 -0400

    refactor: simplify addTwoNumbers function for clarity and efficiency

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -12,22 +12,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        ptr = dummy
+        dummy = ListNode()
+        curr = dummy
         carry = 0
 
         while l1 or l2 or carry:
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-
-            total = x + y + carry
-            carry = total // 10
-
-            ptr.next = ListNode(total % 10)
-            ptr = ptr.next
-
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry = val // 10 if val else 0
+            curr.next = ListNode(val % 10)
+            curr = curr.next
+            if l1:
+                l1 = l1.next            
+            if l2:
+                l2 = l2.next
         
         return dummy.next
 
