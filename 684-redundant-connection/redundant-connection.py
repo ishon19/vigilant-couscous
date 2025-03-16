8ae78c228f3d75754c8af90ccf9c9ecd7617165c
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-03-17 01:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (20.22%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,27 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+    
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        self.parent[self.find(x)] = self.find(y)
+    
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind()
+
+        for x, y in edges:
+            if uf.connected(x, y):
+                return [x, y]
+            uf.union(x, y)
+        
+        return []
