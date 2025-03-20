34ec80512e28f25e5bde4e66cb19bbd9110b965c
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-03-20 18:25:52 -0400

    feat: add solution for rotating an image in-place

---
+++ 48.rotate-image.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=48 lang=python3
+#
+# [48] Rotate Image
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # reverse the rows
+        for i in range(n//2):
+            j = n - i - 1
+            matrix[i], matrix[j] = matrix[j], matrix[i]
+        
+        # take transpose
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+# @lc code=end
+
