0ef4e959c95c6c41845ca6c3dc0538c316177307
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-08 09:55:34 -0400

    fix: correct palindrome check condition to include equal pointers

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -8,15 +8,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         cleanStr = [ch.lower() for ch in s if ch.isalnum()]
-
+        
         l, r = 0, len(cleanStr)-1
 
-        while l < r:
+        while l <= r:
             if cleanStr[l] != cleanStr[r]:
                 return False
             l += 1
             r -= 1
-
+        
         return True
 # @lc code=end
 
