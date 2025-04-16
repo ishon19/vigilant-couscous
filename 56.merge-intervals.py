c1b037c73014cf7496eeeaa36717006632006b43
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-16 04:49:31 -0400

    refactor: improve readability of merge function by using a lambda for overlap checking

--- 56.merge-intervals.py
+++ 56.merge-intervals.py
@@ -9,13 +9,16 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
-
+        
         intervals.sort()
-
         res = [intervals[0]]
+
+        overlaps = lambda last, current: last[1] >= current[0]
+
         for interval in intervals[1:]:
-            if res[-1][1] >= interval[0]:
-                res[-1] = [min(interval[0], res[-1][0]), max(interval[1], res[-1][1])]
+            if overlaps(res[-1], interval):
+                res[-1][0] = min(interval[0], res[-1][0])
+                res[-1][1] = max(interval[1], res[-1][1])
             else:
                 res.append(interval)
         
