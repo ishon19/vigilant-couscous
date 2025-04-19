4a553d36d11b6a7bf5444f6914644d267c804aaf
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-19 08:55:37 -0400

    refactor: optimize variable usage and streamline logic in shortestPathBinaryMatrix method

--- 1091.shortest-path-in-binary-matrix.py
+++ 1091.shortest-path-in-binary-matrix.py
@@ -11,25 +11,24 @@ class Solution:
             return -1
         
         n = len(grid)
+        c = len(grid[0])
+
         dirs = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
 
         q = deque([(0,0,1)])
 
         while q:
             row, col, dist = q.popleft()
-            grid[row][col] = 1
 
+            if row == len(grid)-1 and col == len(grid[0])-1:
+                return dist
+            
             for dr, dc in dirs:
                 nr, nc = row + dr, col + dc
-
-                if nr == n - 1 and nc == n - 1 and grid[nr][nc] != 1:
-                    return dist + 1
-
-                if nr in range(n) and nc in range(n) and grid[nr][nc] == 0:
+                if nr in range(n) and nc in range(c) and grid[nr][nc] != 1:
+                    grid[nr][nc] = 1
                     q.append((nr, nc, dist + 1))
         
         return -1
-
-
 # @lc code=end
 
