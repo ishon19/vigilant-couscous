f3085d70c36cabd88760e98000e963e7bc8bf46b
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-19 09:17:49 -0400

    refactor: optimize result construction in customSortString method

--- 791.custom-sort-string.py
+++ 791.custom-sort-string.py
@@ -8,16 +8,17 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         counter = Counter(s)
-        res = []
+        res = ""
 
         for char in order:
-            if char in counter:
-                res.append(char * counter[char])
-                del counter[char]
+            res += char * counter[char]
+            del counter[char]
+
         
-        for char, count in counter.items():
-            res.append(char * count)
-        
-        return "".join(res)
+        if counter:
+            for key, value in counter.items():
+                res += key * value
+
+        return res
 # @lc code=end
 
