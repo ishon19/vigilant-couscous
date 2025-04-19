7f67439882268eb0adabc6cfbe91b24f668601a3
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-19 08:55:58 -0400

    feat: implement initial structure for distanceK method in all nodes distance K in binary tree

---
+++ 863.all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=863 lang=python3
+#
+# [863] All Nodes Distance K in Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        
+# @lc code=end
+
