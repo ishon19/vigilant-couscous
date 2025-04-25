e9ea91ff44e5991fd7829cabe154fc704433b80a
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-25 00:10:36 -0400

    refactor: streamline addTwoNumbers method for improved readability and efficiency

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -4,6 +4,7 @@
 # [2] Add Two Numbers
 #
 
+
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -12,21 +13,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr = dummy
+        finalList = ListNode()
+        ptr1, ptr2, ptr3 = l1, l2, finalList
         carry = 0
 
-        while l1 or l2 or carry:
-            val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
-            carry = val // 10 if val else 0
-            curr.next = ListNode(val % 10)
-            curr = curr.next
-            if l1:
-                l1 = l1.next            
-            if l2:
-                l2 = l2.next
-        
-        return dummy.next
+        while ptr1 or ptr2 or carry:
+            current_sum = (ptr1.val if ptr1 else 0) + \
+                (ptr2.val if ptr2 else 0) + carry
+            ptr3.next = ListNode(current_sum % 10)
+            carry = current_sum // 10
+            ptr1 = ptr1.next if ptr1 else None
+            ptr2 = ptr2.next if ptr2 else None
+            ptr3 = ptr3.next
 
+        return finalList.next
 # @lc code=end
-
