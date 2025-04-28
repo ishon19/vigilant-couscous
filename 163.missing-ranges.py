699c7cd54ebb43c5dccf69f31ec5dafe36901ee7
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-28 08:19:24 -0400

    refactor: improve comments for clarity in findMissingRanges method

--- 163.missing-ranges.py
+++ 163.missing-ranges.py
@@ -9,24 +9,22 @@ class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         res = []
 
-        # the whole ranges are missing
+        # whole range missing
         if not nums:
             return [[lower, upper]]
-        
-        # check if nums starts with lower
+
+        # check if the starting range has missing numbers
         if nums[0] > lower:
             res = [[lower, nums[0]-1]]
-        
-        # in between the numbers
+
+        # check for missing numbers in between
         for i in range(1, len(nums)):
             if nums[i] - nums[i-1] > 1:
                 res.append([nums[i-1] + 1, nums[i] - 1])
-        
-        # after the last element
-        if nums[-1] < upper:
-            res.append([nums[-1] + 1, upper])
 
+        # check the end
+        if nums[-1] < upper:
+            res.append([nums[-1]+1, upper])
 
         return res
 # @lc code=end
-
