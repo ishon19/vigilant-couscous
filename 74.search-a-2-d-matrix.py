a7475d1886b46e4dc5a86bbb0cf699907e43b69e
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-04-28 08:19:11 -0400

    refactor: simplify variable names and improve readability in searchMatrix method

--- 74.search-a-2-d-matrix.py
+++ 74.search-a-2-d-matrix.py
@@ -8,23 +8,22 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix or not matrix[0]:
-            return False
+            return False 
         
-        rows, cols = len(matrix), len(matrix[0])
-        left, right = 0, rows * cols - 1
+        l, r = 0, len(matrix) * len(matrix[0]) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
-            mid_val = matrix[mid // cols][mid % cols] 
+        while l <= r:
+            mid = (l + r) // 2
+            val = matrix[mid//len(matrix)][mid%len(matrix[0])]
 
-            if mid_val == target:
+            if val == target:
                 return True
-            elif mid_val < target:
-                left = mid + 1
+            elif val > target:
+                r = mid - 1
             else:
-                right = mid - 1
-        
-        return False
+                l = mid + 1
+
+        return False  
     
 # @lc code=end
 
