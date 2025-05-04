8c8264a48abbc95337c13e731bca7646b6dcd117
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-04 14:00:08 -0400

    Time: 177 ms (84.40%) | Memory: 17.8 MB (89.55%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,22 +1,50 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort() 
-        memo = {}
+        # nums.sort() 
+        # memo = {}
 
-        def recurse(index, prev_index):
-            if index == len(nums):
-                return []
+        # def recurse(index, prev_index):
+        #     if index == len(nums):
+        #         return []
             
-            if (index, prev_index) in memo:
-                return memo[(index, prev_index)]
+        #     if (index, prev_index) in memo:
+        #         return memo[(index, prev_index)]
                 
-            included = []
-            if prev_index == -1 or nums[index] % nums[prev_index] == 0:
-                included = [nums[index]] + recurse(index + 1, index)
-            not_included = recurse(index + 1, prev_index)
-            # Choose the longer subset
-            memo[(index, prev_index)] = included if len(included) > len(not_included) else not_included
-            return memo[(index, prev_index)]
+        #     included = []
+        #     if prev_index == -1 or nums[index] % nums[prev_index] == 0:
+        #         included = [nums[index]] + recurse(index + 1, index)
+        #     not_included = recurse(index + 1, prev_index)
+        #     # Choose the longer subset
+        #     memo[(index, prev_index)] = included if len(included) > len(not_included) else not_included
+        #     return memo[(index, prev_index)]
 
-        return recurse(0, -1)
+        # return recurse(0, -1)
+        if not nums:
+            return []
+        
+        nums.sort()
+        n = len(nums)
+
+        dp = [1] * n
+        prev = [-1] * n
+
+        max_size = 1
+        max_idx = 0
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+
+                    if dp[i] > max_size:
+                        max_size = dp[i]
+                        max_idx = i
+        
+        results = []
+        while max_idx>=0:
+            results.append(nums[max_idx])
+            max_idx = prev[max_idx]
+        return results[::-1]
+
 
