99c7660486b7aca8365911ca5d0a473bb491e178
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-04 12:33:54 -0400

    Time: 11 ms (64.49%) | Memory: 17.8 MB (71.03%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,30 +1,18 @@
-"""
-paypalishiring
-
-rows = 4
-
-p     i   n
-a   l s  ig
-y a   h r
-p     i
-
-"""
-
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        if numRows == 1 or numRows >= len(s):
             return s
-
+        
         rows = [''] * numRows
-        goingDown = False
-        curRow = 0
+        row = 0
+        direction = -1
 
         for char in s:
-            rows[curRow] += char            
-            if curRow == 0 or curRow == numRows - 1:
-                goingDown = not goingDown
-            curRow += 1 if goingDown else -1
-        
-        return "".join(rows)
+            rows[row] += char
 
+            if row == 0 or row == numRows-1:
+                direction = -direction
+            
+            row += direction
         
+        return ''.join(rows)
