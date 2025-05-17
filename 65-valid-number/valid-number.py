e8ed558893767f4078966e2719a6134dd93ee86e
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-17 00:16:32 -0400

    Time: 4 ms (10.26%) | Memory: 17.8 MB (38.45%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -8,5 +8,30 @@ The string
 """
 class Solution:
     def isNumber(self, s: str) -> bool:
-        pattern = re.compile(r'^[\s]*[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?[\s]*$')
-        return bool(pattern.fullmatch(s))
+        s = s.strip()
+
+        seen_e = False
+        seen_digit = False 
+        seen_dot = False
+
+        for i, char in enumerate(s):
+            if char.isdigit():
+                seen_digit = True
+            elif char in '+-':
+                if i > 0 and s[i-1] not in 'eE':
+                    return False 
+                if i == len(s) - 1:
+                    return False 
+            elif char == '.':
+                if seen_e or seen_dot:
+                    return False 
+                seen_dot = True
+            elif char in 'eE':
+                if seen_e or not seen_digit:
+                    return False 
+                seen_e = True
+                seen_digit = False 
+            else:
+                return False 
+        
+        return seen_digit 
