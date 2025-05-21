5927875bf5aca7cd2d6d9b2c2fc118a27f2b60e7
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-20 20:28:58 -0400

    Time: 4 ms (64.58%) | Memory: 17.8 MB (40.17%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        charMap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        res = 0        
+        for i in range(len(s)):
+            if i < len(s) - 1 and charMap[s[i+1]] > charMap[s[i]]: 
+                res -= charMap[s[i]]
+            else:
+                res += charMap[s[i]]
+
+        return res
