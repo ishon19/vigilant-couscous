7c4beac5ceb708a7e389c25ae74511957acc9cf8
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-24 11:20:35 -0400

    Time: 7 ms (42.34%) | Memory: 18 MB (20.62%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -3,8 +3,9 @@ class Solution:
         charMap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         
         res = 0        
+        
         for i in range(len(s)):
-            if i < len(s) - 1 and charMap[s[i+1]] > charMap[s[i]]: 
+            if i < len(s) - 1 and charMap[s[i+1]] > charMap[s[i]]:
                 res -= charMap[s[i]]
             else:
                 res += charMap[s[i]]
