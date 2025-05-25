f2eacf07dbae15eea7b4f4242252ab8984da75bd
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-05-25 18:36:48 -0400

    Time: 292 ms (19.17%) | Memory: 20.2 MB (7.36%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,23 +1,24 @@
-from collections import deque
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] != 0 or grid[-1][-1] != 0:
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
-
-        n = len(grid)
-        q = deque([(0, 0, 1)])  # (row, col, distance)
-        visited = set((0, 0))
-
+        
+        rows, cols = len(grid), len(grid[0])
+        q = deque([(0, 0, [(0,0)])])  # (row, col, path)
+        seen = {(0, 0)}
+        
+        directions = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,1), (1,-1), (-1,-1)]
+        
         while q:
-            r, c, dist = q.popleft()
-            if (r, c) == (n - 1, n - 1):
-                return dist
+            row, col, path = q.popleft()
             
-            for dr, dc in [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, -1), (1, -1), (-1, 1)]:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0 and (nr, nc) not in visited:
-                    visited.add((nr, nc))
-                    q.append((nr, nc, dist + 1))
-
+            if row == rows - 1 and col == cols - 1:
+                return len(path)
+            
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and grid[nr][nc] == 0:
+                    seen.add((nr, nc))
+                    q.append((nr, nc, path + [(nr, nc)]))
+        
         return -1
