47f198e7b22bca42e359c690f5a212baecbd971a
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-06-14 11:18:39 -0400

    Time: 3 ms (70.95%) | Memory: 18 MB (44.43%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,52 +1,16 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.isWord = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insert(self, word):
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.isWord = True
-    
-    def searchPrefix(self, s, start):
-        result = []
-        node = self.root
-        for i in range(start, len(s)):
-            char = s[i]
-            if char not in node.children:
-                break
-            node = node.children[char]
-            if node.isWord:
-                result.append(i+1)
-        return result
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        trie = Trie()
-
-        for word in wordDict:
-            trie.insert(word)
-        
-        n = len(s)
-        dp = [False] * (len(s) + 1)
-        dp[0] = True
-
-        for i in range(n+1):
-            if not dp[i]:
-                continue
+        @cache 
+        def dfs(start):
+            if start == len(s):
+                return True 
             
-            for end in trie.searchPrefix(s, i):
-                dp[end] = True
-        
-        return dp[n]
+            for word in wordDict:
+                end = start + len(word)
 
-        t = 0
-
+                if end <= len(s) and s[start:end] == word and dfs(end):
+                    return True 
+            
+            return False 
         
+        return dfs(0)
