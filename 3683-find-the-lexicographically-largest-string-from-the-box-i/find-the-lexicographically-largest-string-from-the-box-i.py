a573c190ea94de9134ffaf958c4c15455bbaf724
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-06-18 08:58:22 -0400

    Time: 22 ms (27.57%) | Memory: 17.8 MB (61.12%) - LeetSync

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        res = ""
+        n = len(word)
+        for i in range(n):
+            res = max(res, word[i: min(i + n - numFriends + 1, n)])
+        return res
+
