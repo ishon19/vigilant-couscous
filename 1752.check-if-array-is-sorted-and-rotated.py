e71df0ad8558d368cd8ca8d88689ec0034438884
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-06-26 11:29:32 -0400

    check how many deflections are there, if more than one, then False, else True

---
+++ 1752.check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=1752 lang=python3
+#
+# [1752] Check if Array Is Sorted and Rotated
+#
+
+# @lc code=start
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = 0
+
+        for i in range(n):
+            if nums[i] > nums[(i+1) % n]:
+                count += 1
+
+                if count > 1:
+                    return False 
+        
+        return True 
+        
+# @lc code=end
+
