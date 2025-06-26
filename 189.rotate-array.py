908bdb226e7d20027cbc427d1df81ce503d73953
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-06-26 10:59:12 -0400

    three reversal approach - first reverse the whole array and then first k and then k to end

---
+++ 189.rotate-array.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=189 lang=python3
+#
+# [189] Rotate Array
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k , n-1)
+
+        
+# @lc code=end
+
