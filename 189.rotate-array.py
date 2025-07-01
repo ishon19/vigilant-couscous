e2150a0a50cb8c96d5268127fc14a10074b7d391
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-07-01 11:03:17 -0400

    messed up the order again

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -10,19 +10,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverse(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
         n = len(nums)
         k = k % n
-
-        def reverse(start, end):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-        
+         
         reverse(0, n-1)
         reverse(0, k-1)
-        reverse(k , n-1)
-
+        reverse(k, n-1) 
         
 # @lc code=end
 
