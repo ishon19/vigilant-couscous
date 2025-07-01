1f91b34377c7d4d11fabccd53e58ec0c143bb52f
Shreyans Pathak <pathak.shreyans@gmail.com>
2025-07-01 11:37:49 -0400

    check the bounds more closely

--- 8.string-to-integer-atoi.py
+++ 8.string-to-integer-atoi.py
@@ -8,25 +8,28 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         res = 0
-        sign = 1
         i = 0
+        sign = 1
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31 
 
         while i < len(s) and s[i] == ' ':
             i += 1
-
+        
         if i == len(s):
             return 0
         
-        if s[i] in ['-', '+']:
-            sign = -1 if s[i] == "-" else 1
+        if s[i] in ['+', '-']:
+            if s[i] == '-':
+                sign = -1
             i += 1
-
+        
         while i < len(s) and s[i].isdigit():
             res = res * 10 + int(s[i])
             i += 1
         
-        res *= sign
+        res = res * sign
+        return min(INT_MAX, max(res, INT_MIN))
 
-        return min(max(res, -2**31), (2 ** 31) -1)
 # @lc code=end
 
