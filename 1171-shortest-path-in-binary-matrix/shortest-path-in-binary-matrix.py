e2130ac52bf8a6d2b6058170bb79d6c48d22aeb0
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-07-12 09:52:08 -0400

    Time: 381 ms (8.02%) | Memory: 19.4 MB (42.58%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,24 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1 or grid[-1][-1] == 1:
+        rows, cols = len(grid), len(grid[0])
+
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1:
             return -1
         
-        rows, cols = len(grid), len(grid[0])
-        q = deque([(0, 0, [(0,0)])])  # (row, col, path)
-        seen = {(0, 0)}
-        
-        directions = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,1), (1,-1), (-1,-1)]
-        
+        q = deque([(0, 0, 1)])
+        dirs = [(1,0),(0,1),(1,1),(-1,0),(0,-1),(1,-1),(-1,1),(-1,-1)]
+        seen = set([(0,0)])
+
         while q:
-            row, col, path = q.popleft()
+            row, col, distance = q.popleft()
+
+            if row == rows-1 and col == cols-1:
+                return distance
             
-            if row == rows - 1 and col == cols - 1:
-                return len(path)
-            
-            for dr, dc in directions:
+            for dr, dc in dirs:
                 nr, nc = row + dr, col + dc
-                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and grid[nr][nc] == 0:
+                if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 0 and (nr, nc) not in seen:
+                    q.append((nr, nc, distance + 1))
                     seen.add((nr, nc))
-                    q.append((nr, nc, path + [(nr, nc)]))
-        
+
         return -1
+
