820e30c709423d5fa1017ab5ecb8590043ac850b
Shreyans Pathak <ishon19@users.noreply.github.com>
2025-10-12 13:53:01 -0400

    Time: 299 ms (15.87%) | Memory: 23.9 MB (16.42%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,9 +1,9 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.is_word = False
+        self.children: Dict[str, TrieNode] = {}
+        self.is_end = False
     
-class AutoComplete:
+class Trie:
     def __init__(self):
         self.root = TrieNode()
     
@@ -13,40 +13,41 @@ class AutoComplete:
             if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-        node.is_word = True
-    
-    def find_suggestions(self, prefix):
+        node.is_end = True
+
+    def get_suggestions(self, prefix):
         node = self.root
         for char in prefix:
             if char not in node.children:
                 return []
             node = node.children[char]
         
+        # reached the end of the prefix, now build up suggestions
         suggestions = []
         self.dfs(node, prefix, suggestions)
-        return suggestions[:3]
-        
-    def dfs(self, node, prefix, suggestions):
+        return suggestions
+    
+    def dfs(self, node, current, suggestions):
         if len(suggestions) == 3:
-            return 
+            return suggestions
 
-        if node.is_word:
-            suggestions.append(prefix)
+        if node.is_end:
+            suggestions.append(current)
         
         for char, child in node.children.items():
-            self.dfs(child, prefix + char, suggestions)
+            self.dfs(child, current + char, suggestions)
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        auto_complete = AutoComplete()
-
+        trie = Trie()
         for product in sorted(products):
-            auto_complete.insert(product)
+            trie.insert(product)
         
         res = []
-        prefix = ""
-        for char in searchWord:
-            prefix += char
-            suggestions = auto_complete.find_suggestions(prefix)
-            res.append(suggestions)
+        prefix = ''
+
+        for letter in searchWord:
+            prefix += letter
+            res.append(trie.get_suggestions(prefix))
+
         return res
